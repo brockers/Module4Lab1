bb7e5ce3abeb4446debd9b41a0738716b05bca3e
Bob Rockers <bob@rockerssoft.org>
2015-05-19 19:08:05 -0500

    Updated to 5.2 solution

--- CriminalIntentSolution/src/com/example/criminalintentsolution/CrimeLab.java
+++ CriminalIntentSolution/src/com/example/criminalintentsolution/CrimeLab.java
@@ -4,16 +4,27 @@ import java.util.ArrayList;
 import java.util.UUID;
 
 import android.content.Context;
+import android.util.Log;
 
 public class CrimeLab {
+	private static final String TAG = "CrimeLab";
+	private static final String FILENAME = "crimes.json";
 	private static CrimeLab sCrimeLab;
 	private Context mAppContext;
 	private ArrayList<Crime> mCrimes;
+	private CriminalIntentJSONSerializer mSerializer;
 	
 	private CrimeLab(Context appContext) {
 		mAppContext = appContext;
-		mCrimes = new ArrayList<Crime>();
 		
+		mSerializer = new CriminalIntentJSONSerializer(mAppContext, FILENAME);
+		
+		try {
+			mCrimes = mSerializer.loadCrimes();
+			
+		}catch (Exception e){
+			mCrimes = new ArrayList<Crime>();
+		}
 	/*	for(int i=0; i<100; i++) {
 			Crime c = new Crime();
 			c.setTitle("Crime#" + i);
@@ -24,6 +35,19 @@ public class CrimeLab {
 	
 	public void addCrime(Crime c) {
 		mCrimes.add(c);
+	}
+	
+	public boolean saveCrimes() {
+		try {
+			mSerializer.saveCrimes(mCrimes);
+			Log.d(TAG, "crimes saved to file");
+			return true;
+		}
+		catch (Exception e) {
+			Log.e(TAG, "Error saving crimes: ", e);
+			return false;
+			
+		}
 	}
 	public static CrimeLab get(Context c) {
 		if (sCrimeLab==null) {
